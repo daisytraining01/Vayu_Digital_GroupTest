8b9f71d0803451fd7233ead02f1fefd6f06b93f5
Vimal Raj <vimalrajv008@gmail.com>
2020-06-09 14:38:03 +0530

    Nisha Branch

---
+++ src/test/java/com/pageobjectrepository/APIDemos_Nisha.java
@@ -0,0 +1,16 @@
+package com.pageobjectrepository;
+
+import org.openqa.selenium.By;
+
+public class APIDemos_Nisha {
+
+	public static final By Views = By.xpath("//android.widget.TextView[@content-desc='Views']");
+	public static final By Chronometer = By.xpath("//android.widget.TextView[@content-desc='Chronometer']");
+	public static final By start = By.id("io.appium.android.apis:id/start");
+	public static final By stop = By.id("io.appium.android.apis:id/stop");
+	public static final By Animation = By.xpath("//android.widget.TextView[@content-desc='Animation']");
+	public static final By Multiple_Properties = By
+			.xpath("//android.widget.TextView[@content-desc='Multiple Properties']");
+	public static final By startButton = By.id("io.appium.android.apis:id/startButton");
+
+}
---
+++ src/test/java/com/testscenerios/APIDemo_Nisha.java
@@ -0,0 +1,110 @@
+package com.testscenerios;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import io.appium.java_client.AppiumDriver;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+
+import com.pageobjectrepository.APIDemos_Nisha;
+import com.utilities.Listener_Vimal;
+
+@Listeners(com.utilities.Listener_Vimal.class)
+public class APIDemo_Nisha extends APIDemos_Nisha {
+	public static AppiumDriver driver;
+
+	@Parameters({ "ModeOfRun" })
+	@BeforeTest
+	public void setUp(String ModeOfRun) throws MalformedURLException {
+
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		if (ModeOfRun.contentEquals("Emulator")) {
+
+			capabilities.setCapability("deviceName", "Pixel");
+			capabilities.setCapability("udid", "emulator-5554");
+			capabilities.setCapability("platformVersion", "7.0");
+			capabilities.setCapability("automationName", "UiAutomator1");
+			capabilities.setCapability("platformName", "ANDROID");
+			capabilities.setCapability("appPackage", "io.appium.android.apis");
+			capabilities.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
+			URL url = new URL("http://127.0.0.1:4723/wd/hub");
+			driver = new AndroidDriver<AndroidElement>(url, capabilities);
+
+		}
+
+		else if (ModeOfRun.contentEquals("Cloud")) {
+
+			String accessKey = "eyJ4cC51Ijo0NTU2OTcsInhwLnAiOjQ1NTY4OSwieHAubSI6Ik1UVTRPVE0yTnprMU1EQXlNdyIsImFsZyI6IkhTMjU2In0.eyJleHAiOjE5MDQ3Mjc5NTAsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.9PEKvslTXNVQjibm_oLIBni8iknsIwo1etQfjBMQ8ME";
+			capabilities.setCapability("accessKey", accessKey);
+			capabilities.setCapability("deviceQuery", "@os='android' and @category='PHONE'");
+			capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "io.appium.android.apis");
+			capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "io.appium.android.apis.ApiDemos");
+			driver = new AndroidDriver(new URL("https://demo.experitest.com/wd/hub"), capabilities);
+
+		}
+
+		else if (ModeOfRun.contentEquals("Real")) {
+
+			capabilities.setCapability("deviceName", "Nish");
+			capabilities.setCapability("udid", "JCAAGF06C5578SZ");
+			capabilities.setCapability("automationName", "UiAutomator1");
+			capabilities.setCapability("platformName", "Android");
+			capabilities.setCapability("platformVersion", "9");
+			capabilities.setCapability("appPackage", "io.appium.android.apis");
+			capabilities.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
+			capabilities.setCapability("automationName", "UiAutomator1");
+			URL remoteUrl = new URL("http://localhost:4723/wd/hub");
+			driver = new AndroidDriver(remoteUrl, capabilities);
+
+		}
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+	}
+
+	@Test
+	public void Chronometer_Nisha() throws InterruptedException {
+		driver.findElement(Views).click();
+		Thread.sleep(5000);
+		driver.findElement(Chronometer).click();
+		Thread.sleep(5000);
+		driver.findElement(start).click();
+		Thread.sleep(5000);
+		driver.findElement(stop).click();
+		Thread.sleep(5000);
+
+	}
+
+	@Test
+	public void Animation_Nisha() throws InterruptedException {
+		driver.findElement(Animation).click();
+		Thread.sleep(5000);
+		driver.findElement(Multiple_Properties).click();
+		Thread.sleep(5000);
+		driver.findElement(startButton).click();
+		Thread.sleep(5000);
+
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+
+	}
+}
