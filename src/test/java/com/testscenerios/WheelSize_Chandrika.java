05bc23134c90674af224298ea15557037e711f0d
daisytraining01 <64245212+daisytraining01@users.noreply.github.com>
2020-06-10 15:38:42 +0530

    Update WheelSize_Chandrika.java

--- src/test/java/com/testscenerios/WheelSize_Chandrika.java
+++ src/test/java/com/testscenerios/WheelSize_Chandrika.java
@@ -1,37 +1,33 @@
 package com.testscenerios;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.ITestContext;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import com.pageobjectrepository.WordWeb_Homepage_Suresh;
-import com.utilities.Action_Vimal;
-import com.utilities.Excel_Utility_Vimal;
-import com.utilities.Screenshot_Vimal;
-
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.android.nativekey.AndroidKey;
-import io.appium.java_client.android.nativekey.KeyEvent;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 
+import com.pageobjectrepository.*;
+import com.utilities.*;
+
 @Listeners(com.utilities.Listener_Vimal.class)
-public class WordWeb_Suresh extends WordWeb_Homepage_Suresh {
+public class WheelSize_Chandrika extends WheelSize_Homepage_Chandrika {
+
 	public static AndroidDriver driver;
-	Action_Vimal action;
 	Screenshot_Vimal scren;
+	Action_Vimal action;
 
 	@BeforeClass
 	public void objectCreation() {
@@ -39,7 +35,7 @@ public class WordWeb_Suresh extends WordWeb_Homepage_Suresh {
 	}
 
 	@Parameters({ "ModeOfRun" })
-	@BeforeMethod
+	@BeforeTest
 	public void setUp(String ModeOfRun) throws MalformedURLException {
 
 		DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -50,8 +46,8 @@ public class WordWeb_Suresh extends WordWeb_Homepage_Suresh {
 			capabilities.setCapability("platformVersion", "7.0");
 			capabilities.setCapability("automationName", "UiAutomator1");
 			capabilities.setCapability("platformName", "ANDROID");
-			capabilities.setCapability("appPackage", "com.wordwebsoftware.android.wordweb");
-			capabilities.setCapability("appActivity", "com.wordwebsoftware.android.wordweb.activity.WordWebActivity");
+			capabilities.setCapability("appPackage", "com.wheelsize");
+			capabilities.setCapability("appActivity", "com.wheelsize.presentation.container.AppContainer");
 			URL url = new URL("http://127.0.0.1:4723/wd/hub");
 			driver = new AndroidDriver<AndroidElement>(url, capabilities);
 
@@ -62,64 +58,56 @@ public class WordWeb_Suresh extends WordWeb_Homepage_Suresh {
 			String accessKey = "eyJ4cC51Ijo0NTU2OTcsInhwLnAiOjQ1NTY4OSwieHAubSI6Ik1UVTRPVE0yTnprMU1EQXlNdyIsImFsZyI6IkhTMjU2In0.eyJleHAiOjE5MDQ3Mjc5NTAsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.9PEKvslTXNVQjibm_oLIBni8iknsIwo1etQfjBMQ8ME";
 			capabilities.setCapability("accessKey", accessKey);
 			capabilities.setCapability("deviceQuery", "@os='android' and @category='PHONE'");
-			capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.avrapps.emicalculator");
-			capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".activities.EMICalculator");
+			capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.wheelsize");
+			capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,
+					"com.wheelsize.presentation.container.AppContainer");
 			driver = new AndroidDriver(new URL("https://demo.experitest.com/wd/hub"), capabilities);
 
 		}
 
 		else if (ModeOfRun.contentEquals("Real")) {
 
-			capabilities.setCapability("deviceName", "Nish");
-			capabilities.setCapability("udid", "JCAAGF06C5578SZ");
-			capabilities.setCapability("automationName", "UiAutomator1");
+			capabilities.setCapability("deviceName", "Redmi Note Pro");
 			capabilities.setCapability("platformName", "Android");
-			capabilities.setCapability("platformVersion", "9");
-			capabilities.setCapability("appPackage", "com.avrapps.emicalculator");
-			capabilities.setCapability("appActivity", ".activities.EMICalculator");
+			capabilities.setCapability("platformVersion", "8.0");
+			capabilities.setCapability("udid", "uwgidmk7beyhq8eq");
+			capabilities.setCapability("appPackage", "com.wheelsize");
+			capabilities.setCapability("appActivity", "com.wheelsize.presentation.container.AppContainer");
 			capabilities.setCapability("automationName", "UiAutomator1");
+			capabilities.setCapability("app", "C:\\Users\\ranganath\\Downloads\\WheelSize_v1.1.2_apkpure.com.apk");
 			URL remoteUrl = new URL("http://localhost:4723/wd/hub");
 			driver = new AndroidDriver(remoteUrl, capabilities);
+
 		}
 
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
 	}
 
-	@Test(dataProvider = "TestDataFromExcel")
-	public void createBookmark(String keyword, final ITestContext testContext) throws Exception {
-		action.Click(clikCheckatabase);
-		Thread.sleep(50000);
-		action.ClearAndSendKeys(searchText, keyword);
-		action.Click(clickFirstResult);
-		action.Click(bookMark);
-		action.Click(backButton);
-		Assert.assertTrue(action.isDisplayed(lookup));
+	@Test
+	public void WheelappDemo(final ITestContext testContext) throws Exception {
+		action = new Action_Vimal(driver);
+		driver.findElement(make).click();
+		Thread.sleep(10000);
+		driver.findElement(makePath).click();
+		Thread.sleep(10000);
+		driver.findElement(year).click();
+		Thread.sleep(10000);
+		driver.findElement(model).click();
+		Thread.sleep(10000);
+		driver.findElement(trim).click();
+		Thread.sleep(5000);
+		String str = driver.findElement(title).getText();
+		System.out.println(str);
 		scren.takeSnapShot(driver, testContext.getName());
+		Assert.assertEquals(str, "2019 Audi A1, 30 TFSi [GB]");
 
 	}
 
-	@Test(dataProvider = "TestDataFromExcel")
-	public void Search(String Keyword, final ITestContext testContext) throws Exception {
-		action.Click(clikCheckatabase);
-		Thread.sleep(50000);
-		action.Click(searchTab);
-		action.Click(noun);
-		action.ClearAndSendKeys(searchText, Keyword);
-		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
-		action.Click(clickFirstResultType);
-		action.Click(backButton);
-		action.Click(lookup);
-		Assert.assertTrue(action.isDisplayed(lookup));
-		scren.takeSnapShot(driver, testContext.getName());
-
-	}
-
-	@DataProvider
-	public Object[][] TestDataFromExcel() throws IOException {
-
-		return Excel_Utility_Vimal.getTestData("WordWeb.xlsx");
-
+	@AfterTest
+	public void WheelappDemo1() {
+		System.out.println("Wheel Details");
+		driver.quit();
 	}
 
 }
